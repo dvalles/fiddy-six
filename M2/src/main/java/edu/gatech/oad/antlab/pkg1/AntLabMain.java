b1cfc983325f814516a83e4ee94c4b1a4f6691d2
Dom <domvalles@gmail.com>
2015-01-22 06:59:11 -0800

    Changed all the changes I made to person 5 to person 6.

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -78,13 +78,13 @@ import edu.gatech.oad.antlab.pkg2.*;
                   
                   //Person5 replace P5 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Domenico Valles");
-                  toPrint += p5.toString("dvalles3");
+                  Person5 p5 = new Person5("P4");
+                  toPrint += p5.toString("gburdell5");
                   
                   //Person6 replace P6 with your name
                   //and gburdell6 with your gt id
-                  Person6 p6 = new Person6("P6");
-                  toPrint += p6.toString("gburdell6");
+                  Person6 p6 = new Person6("Domenico Valles");
+                  toPrint += p6.toString("dvalles3");
 		  
         System.out.println(toPrint);
         
