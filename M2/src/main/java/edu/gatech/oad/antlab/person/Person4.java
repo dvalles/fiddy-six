0378b032a92b620c45353f328c493c38025fb928
jonathanzwhite <jonathanwhite.dev@gmail.com>
2015-01-26 15:47:45 -0500

    Revisions

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string
  *
- *  @author Bob
+ *  @author Ryan Brooks
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,15 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+          //Person 4 put your implementation here
+        char[] string = input.toCharArray();
+        String answer = "";
+        for (int i = 0; i < string.length; i++) {
+          string[i] = ++string[i];
+          answer += string[i];
+        }
+
+        return answer;
     }
 
     /**
